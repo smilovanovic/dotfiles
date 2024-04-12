314a4adfcade889e9ad6941feeeadeaf403058bf
Stefan Milovanovic <stefan@Stefans-MacBook-Pro.local>
2024-04-12 23:34:41 +0200

    feat: wezterm logs cd

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -9,6 +9,9 @@ return {
       lua_ls = {
         settings = {
           Lua = {
+            runtime = {
+              version = "LuaJIT",
+            },
             diagnostics = {
               globals = { "vim" },
             },
